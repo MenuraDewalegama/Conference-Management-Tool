1c193ab383f2d272734ec583d416cc88a1405a69
DhanushaPerera07 <buddhika.dhanusha@gmail.com>
2021-05-25 20:49:27 +0530

    Config basic component to de displayed.

---
+++ Conference-Management-Tool-app-frontend/src/components/conference/ConferenceHolder.jsx
@@ -0,0 +1,17 @@
+/*
+@author : Dhanusha Perera
+@date : 25/05/2021
+*/
+import React from 'react';
+
+/* functional component. */
+const ConferenceHolder = () => {
+    return (
+        <div>
+            <h1>Conference holder works!</h1>
+        </div>
+    );
+};
+
+export default ConferenceHolder;
+
