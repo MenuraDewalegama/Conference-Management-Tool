07082b2bb9765ed3eff32a929f69c5bc9aa915c3
DhanushaPerera07 <buddhika.dhanusha@gmail.com>
2021-05-27 00:06:00 +0530

    Add DATABASE_NAME property in .env file and Add conference-post.api.js file.

---
+++ Conference-Management-Tool-app-backend/src/api/conference-post.api.js
@@ -0,0 +1,39 @@
+/*
+@author : Dhanusha Perera
+@date : 26/05/2021
+*/
+
+/* get all conference posts. */
+const getAllConferencePost = () => {
+    return new Promise((resolve, reject) => {
+
+    });
+};
+
+/* get a conference post by ID. */
+const getConferencePostByID = (conferencePostID) => {
+    return new Promise((resolve, reject) => {
+
+    });
+};
+
+/* creates a new save conference post. */
+const saveConferencePost = (conferencePost) => {
+    return new Promise((resolve, reject) => {
+
+    });
+};
+
+/* update a conference post. */
+const updateConferencePost = (id, conferencePost) => {
+    return new Promise((resolve, reject) => {
+
+    });
+};
+
+/* delete a conference post by ID. */
+const deleteConferencePost = (conferencePostID) => {
+    return new Promise((resolve, reject) => {
+
+    });
+};
