eae7cffd5fbb15cc5f0f8a247955e10681c6b963
janaka chinthana <janakachinthana1@gmail.com>
2021-05-26 21:18:49 +0530

    DashBoard Class Initiated closed #4

--- Conference-Management-Tool-app-frontend/src/components/dashBoard/DashBoard.jsx
+++ Conference-Management-Tool-app-frontend/src/components/dashBoard/DashBoard.jsx
@@ -1,9 +1,23 @@
+/*
+@author : Janaka Chinthana
+@date : 26/05/2021
+*/
+
 import React from 'react';
+import {Col, Container, Image, Row} from 'react-bootstrap';
+import './DashBoard.css';
 
 export default class DashBoard extends React.Component{
 
     constructor(props){
         super(props);
+    }
+
+    render(){
+        return <div>
+            <br />
+           <h1 className="center">Welcome to DashBoard</h1>
+        </div>
     }
 
 }
