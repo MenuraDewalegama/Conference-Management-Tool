e2c79c4e10bd10698ed0822c6c044187892b2aea
DhanushaPerera07 <buddhika.dhanusha@gmail.com>
2021-05-27 00:41:42 +0530

    Add and Config conference-post.dao.js CRUD functions.

---
+++ Conference-Management-Tool-app-backend/src/dal/conference-post-dao.js
@@ -0,0 +1,47 @@
+/*
+@author : Dhanusha Perera
+@date : 27/05/2021
+*/
+
+/* get all conference posts. */
+const getAllConferencePost = () => {
+    return new Promise((resolve, reject) => {
+
+    });
+};
+
+/* get a conference post by ID. */
+const getConferencePost = (conferencePost) => {
+    return new Promise((resolve, reject) => {
+
+    });
+};
+
+/* create a new conference post. */
+const createConferencePost = () => {
+    return new Promise((resolve, reject) => {
+
+    });
+};
+
+/* update a conference post. */
+const updateConferencePost = () => {
+    return new Promise((resolve, reject) => {
+
+    });
+};
+
+/*delete a conference post. */
+const deleteConferencePost = () => {
+    return new Promise((resolve, reject) => {
+
+    });
+};
+
+module.exports = {
+    getAllConferencePost,
+    getConferencePost,
+    createConferencePost,
+    updateConferencePost,
+    deleteConferencePost
+};
