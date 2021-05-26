de491df1aa72a9053c9c97d45b0d9931f79070e5
DhanushaPerera07 <buddhika.dhanusha@gmail.com>
2021-05-26 14:34:17 +0530

    Config databse.util.js.

--- Conference-Management-Tool-app-backend/src/util/database.util.js
+++ Conference-Management-Tool-app-backend/src/util/database.util.js
@@ -2,3 +2,24 @@
 @author : Dhanusha Perera
 @date : 26/05/2021
 */
+const {MongoClient} = require('mongodb');
+const dotenv = require('dotenv').config();
+
+if (dotenv.error) {
+    throw dotenv.error;
+}
+
+const DATABASE = process.env.DATABASE;
+
+const client = new MongoClient(DATABASE, {useNewUrlParser: true, useUnifiedTopology: true});
+
+client.connect(err => {
+    if (err) {
+        console.log(err);
+        process.exit(-1);
+    }
+    console.log("Connected to the MongoDB Successfully.")
+});
+
+module.exports = client;
+
