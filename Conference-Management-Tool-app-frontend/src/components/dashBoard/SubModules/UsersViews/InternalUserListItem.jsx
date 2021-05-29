a76af535e15cb513aaf89eec0984c3198da65222
Janaka Chinthana <janakachinthana1@gmail.com>
2021-05-29 16:04:50 +0530

    implemented InternalUserListItem class and internal Users

---
+++ Conference-Management-Tool-app-frontend/src/components/dashBoard/SubModules/UsersViews/InternalUserListItem.jsx
@@ -0,0 +1,30 @@
+import React, { useState } from 'react';
+import { CartPlus, PencilSquare } from 'react-bootstrap-icons';
+import { Link, useHistory } from 'react-router-dom';
+import sha256 from 'crypto-js/sha256';
+import { toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+
+export default function InternalUserList(props) {
+
+    const { internalUser } = props;
+    let history = useHistory();
+
+    return (
+        <div style={{ marginRight: '5%' }}>
+            <br />
+            <div className="row">
+                <div className="col">
+                    <div className="card" style={{ width: '20rem', height: '40rem' }}>
+                        <div className="card-body">
+                            <Card.Title><b>{internalUser.fullName}</b></Card.Title>
+                            <Card.Text style={fontStyle}>
+                                contactNo: {internalUser.contactNo}
+                            </Card.Text>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+}
--- Conference-Management-Tool-app-frontend/src/components/dashBoard/SubModules/UsersViews/InternalUsers.jsx
+++ Conference-Management-Tool-app-frontend/src/components/dashBoard/SubModules/UsersViews/InternalUsers.jsx
@@ -1,4 +1,3 @@
 import React from "react";
-
 import { Link } from "react-router-dom";
 import sha256 from "crypto-js/sha256";
