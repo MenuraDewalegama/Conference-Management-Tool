6aca389cff1f44e79b923bdaa19856a031a0617d
DhanushaPerera07 <buddhika.dhanusha@gmail.com>
2021-05-30 13:15:01 +0530

    Reformat code in app.js

--- Conference-Management-Tool-app-backend/app.js
+++ Conference-Management-Tool-app-backend/app.js
@@ -1,32 +1,32 @@
-const Koa = require("koa");
-const fs = require("fs");
-const path = require("path");
-const koaBody = require("koa-body");
-const jwt = require("jsonwebtoken");
-const koaJWT = require("koa-jwt");
-const unless = require("koa-unless");
-const ms = require("ms");
-const dotenv = require("dotenv").config();
-require("./src/util/database.util");
-const directoryService = require("./src/service/dir.service");
+const Koa = require('koa');
+const fs = require('fs');
+const path = require('path');
+const koaBody = require('koa-body');
+const jwt = require('jsonwebtoken');
+const koaJWT = require('koa-jwt');
+const unless = require('koa-unless');
+const ms = require('ms');
+const dotenv = require('dotenv').config();
+require('./src/util/database.util');
+const directoryService = require('./src/service/dir.service');
 
 const app = new Koa();
 const PORT = process.env.dPORT;
 
 /* enable CORS. */
-const cors = require("@koa/cors");
+const cors = require('@koa/cors');
 app.use(cors());
 
 /* enable koa-body. */
-app.use(koaBody({ multipart: true }));
+app.use(koaBody({multipart: true}));
 
 /* create assets dir if not exists. */
 directoryService.createDirIfNotExists();
 
 /* routes. */
-const conferencePostRoutes = require("./src/routes/conference-post.route");
+const conferencePostRoutes = require('./src/routes/conference-post.route');
 // console.log(conferencePostRoutes);
-const InternalUserRoutes = require("../Conference-Management-Tool-app-backend/src/routes/internalUser.routes");
+const InternalUserRoutes = require('../Conference-Management-Tool-app-backend/src/routes/internalUser.routes');
 
 /* auth route and assets routes are exposed here. */
 
@@ -35,17 +35,15 @@ const InternalUserRoutes = require("../Conference-Management-Tool-app-backend/sr
 /* JWT protected routes should place under this line. */
 
 /* app routes are set here. */
-app
-  .use(conferencePostRoutes.routes())
-  .use(conferencePostRoutes.allowedMethods());
+app.use(conferencePostRoutes.routes()).use(conferencePostRoutes.allowedMethods());
 
 app.use(InternalUserRoutes.routes()).use(InternalUserRoutes.allowedMethods());
 
 /* server. */
 app.listen(PORT, (error) => {
-  if (error) {
-    console.error("Something went wrong when server starts!");
-  } else {
-    console.log(`Server is started on port ${PORT}`);
-  }
+    if (error) {
+        console.error('Something went wrong when server starts!');
+    } else {
+        console.log(`Server is started on port ${PORT}`);
+    }
 });
