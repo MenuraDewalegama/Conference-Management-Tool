19e63e9a8e48c67c7b716de3f97e5b01529540ec
DhanushaPerera07 <buddhika.dhanusha@gmail.com>
2021-06-18 00:36:50 +0530

    Export getAll getByID methods in conference-post.service.js file.

--- Conference-Management-Tool-app-frontend/src/service/conference-post.service.js
+++ Conference-Management-Tool-app-frontend/src/service/conference-post.service.js
@@ -2,3 +2,38 @@
 @author : Dhanusha Perera
 @date : 17/06/2021
 */
+
+import axios from 'axios';
+
+const baseURL = process.env.CONFERENCE_MANAGEMENT_BACKEND_API_URL;
+
+const getAllConferencePosts = () => {
+    return new Promise((resolve, reject) => {
+        return new Promise(async (resolve, reject) => {
+            try {
+                // If the given base URL or the resulting URL are not valid URLs, the JavaScript TypeError exception is thrown.
+                const getAllConferencePostsURL = new URL(`/api/v1/conferences`, baseURL);
+                resolve(await axios.get(getAllConferencePostsURL.href));
+            } catch (error) {
+                reject(error);
+            }
+        });
+    });
+};
+
+const getConferencePostByID = (conferencePostID) => {
+    return new Promise(async (resolve, reject) => {
+        try {
+            // If the given base URL or the resulting URL are not valid URLs, the JavaScript TypeError exception is thrown.
+            const getConferencePostByIDURL = new URL(`/api/v1/conferences/${conferencePostID}`, baseURL);
+            resolve(await axios.get(getConferencePostByIDURL.href));
+        } catch (error) {
+            reject(error);
+        }
+    });
+};
+
+module.exports = {
+    getAllConferencePosts,
+    getConferencePostByID
+};
