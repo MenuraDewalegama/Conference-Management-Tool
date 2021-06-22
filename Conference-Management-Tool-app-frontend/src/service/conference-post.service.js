f68e09146d17fd80adf29932df7f7c283bce1e15
DhanushaPerera07 <buddhika.dhanusha@gmail.com>
2021-06-22 22:28:49 +0530

    Add save,update, delete service methods for conference-posts.(not tested yet)

--- Conference-Management-Tool-app-frontend/src/service/conference-post.service.js
+++ Conference-Management-Tool-app-frontend/src/service/conference-post.service.js
@@ -33,7 +33,74 @@ const getConferencePostByID = (conferencePostID) => {
     });
 };
 
+
+const saveConferencePost = (conferencePost) => {
+    return new Promise(async (resolve, reject) => {
+        try {
+            // If the given base URL or the resulting URL are not valid URLs, the JavaScript TypeError exception is thrown.
+            const postConferencePostURL = new URL(`/api/v1/conferences`, baseURL);
+
+            /* send a post request to the backend using axios. */
+            resolve(await axios.post(postConferencePostURL.href, conferencePost));
+        } catch (error) {
+            reject(error);
+        }
+
+    });
+};
+
+const updateConferencePost = (conferencePost) => {
+    return new Promise(async (resolve, reject) => {
+        try {
+            // If the given base URL or the resulting URL are not valid URLs, the JavaScript TypeError exception is thrown.
+            const putConferencePostURL = new URL(`/api/v1/conferences/${conferencePost?.id}`, baseURL);
+
+            /* send a put request to the backend using axios. */
+            const response = axios.put(putConferencePostURL.href, conferencePost);
+            resolve(response);
+        } catch (error) {
+            if (error.response) {
+                // The request was made and the server responded with a status code
+                // that falls out of the range of 2xx
+                console.log(error.response.data);
+                console.log(error.response.status);
+                console.log(error.response.headers);
+            } else if (error.request) {
+                // The request was made but no response was received
+                // `error.request` is an instance of XMLHttpRequest in the browser and an instance of
+                // http.ClientRequest in node.js
+                console.log(error.request);
+            } else {
+                // Something happened in setting up the request that triggered an Error
+                console.log('Error', error.message);
+            }
+            console.log(error.config);
+            reject(error);
+        }
+    });
+};
+
+
+const deleteConferencePost = (conferencePostID) => {
+    return new Promise((resolve, reject) => {
+        try {
+            // If the given base URL or the resulting URL are not valid URLs, the JavaScript TypeError exception is thrown.
+            const deleteConferencePostURL = new URL(`/api/v1/conferences/${conferencePostID}`, baseURL);
+
+            /* send a delete request to the backend using axios. */
+            const result = axios.delete(deleteConferencePostURL.href);
+            resolve(result);
+        } catch (error) {
+            reject(error);
+        }
+    });
+};
+
+
 module.exports = {
     getAllConferencePosts,
-    getConferencePostByID
+    getConferencePostByID,
+    saveConferencePost,
+    updateConferencePost,
+    deleteConferencePost
 };
