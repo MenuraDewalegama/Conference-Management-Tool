53a0f86f0faca5b87f70433c75dbbd8b6b2f56ec
Janaka Chinthana <janakachinthana1@gmail.com>
2021-06-30 13:35:06 +0530

    set index file to use external user context

--- Conference-Management-Tool-app-frontend/index.jsx
+++ Conference-Management-Tool-app-frontend/index.jsx
@@ -1,17 +1,20 @@
 import React from 'react';
-import {render} from 'react-dom';
+import { render } from 'react-dom';
 import App from './App';
-import {UserProvider} from './src/context/user.context';
-import {InternalUserProvider} from './src/context/internalUser.context';
-import {ConferencePostProvider} from './src/context/conference-post.context';
-import {ConferencePostAddEditProvider} from './src/context/conference-post-add-edit.context';
+import { UserProvider } from './src/context/user.context';
+import { InternalUserProvider } from './src/context/internalUser.context';
+import { ExternalUserProvider } from './src/context/externalUser.context';
+import { ConferencePostProvider } from './src/context/conference-post.context';
+import { ConferencePostAddEditProvider } from './src/context/conference-post-add-edit.context';
 
 render(
     <ConferencePostAddEditProvider><ConferencePostProvider>
-        <InternalUserProvider>
-            <UserProvider>
-                <App/>
-            </UserProvider>
-        </InternalUserProvider>
+        <ExternalUserProvider>
+            <InternalUserProvider>
+                <UserProvider>
+                    <App />
+                </UserProvider>
+            </InternalUserProvider>
+        </ExternalUserProvider>
     </ConferencePostProvider></ConferencePostAddEditProvider>
     , document.getElementById('app'));
