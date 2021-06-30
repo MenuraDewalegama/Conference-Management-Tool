f9fb2444a1ecdae219450b4155cd4789f0fec1ec
sachinthazoysa <55564245+sachinthazoysa@users.noreply.github.com>
2021-06-30 08:25:22 +0530

    testing implemented for external user API endpoint

---
+++ Conference-Management-Tool-app-backend/test/api/externaluser-api.test.js
@@ -0,0 +1,23 @@
+const koa = require("koa"); // import koa
+const serverRoutes = require("../../app"); //import file we are testing
+const request = require("supertest"); // supertest is a framework that allows to easily test web apis
+const app =new koa(); //an instance of an koa app, a 'fake' express app
+// app.use("/members", serverRoutes); //routes
+
+const membersRoutes = require('../routes/members.routes');
+app.use(membersRoutes.routes()).use(membersRoutes.allowedMethods());
+
+
+describe("testing-committe-member-endpoints", () => {
+  it("GET /members - success", async () => {
+    const { body } = await request(app).get("/members"); //uses the request function that calls on express app instance
+    expect(body).toEqual([
+      {
+        name: "Mr. Sri Vaas",
+        designation: "Chairman - Virtusa",
+        information: "Well recognized IT speaker in Sri Lanka",
+        imagePath:'/assets/members/60db44d30196cc1fa8a1ce0b.jpeg'
+      }
+    ]);
+  });
+});
